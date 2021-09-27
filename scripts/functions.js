94441695b23312cb067f6b20a865e3a324cb04bc
Matias Di Iorio <matiasmd8610@gmail.com>
2021-09-27 18:31:27 -0300

    Initialize project

---
+++ scripts/functions.js
@@ -0,0 +1,211 @@
+//FUNCION QUE CONSULTA POR LA EDAD DE LA PERSONA PARA DEJARLA INGRESAR AL SITIO EN CASO QUE SEA MAYOR DE EDAD
+const restrictionAge = (age) => {
+    if (parseInt(age) >= 18) {
+        alert("¡Bienvenido!");
+    } else if (age == null) {
+        window.location.href = "https://kids.nationalgeographic.com/";
+    } else {
+        alert("Usted es menor de edad y no puede operar en esta página");
+        window.location.href = "https://kids.nationalgeographic.com/";
+    }
+}
+
+//FUNCIÓN QUE CARGA TODO EL CATALOGO DE VINOS DE LA SECCIÓN NUESTROS VINOS
+const buildCatalog = (arr) => {
+    let html ='';
+    arr.forEach(product => {
+            //console.log(productsContainer);
+            html += `<div class="product">`;
+            if (product.discount > 0) {
+                html += `<span class="discount">${product.discount}% OFF</span>` //ALT + 96
+            }
+            html +=`<figure>
+                         <img src="${product.imageURL}" width="100" alt="${product.name}">
+                    </figure>
+                    <h4>${product.name}</h4>
+                    <span class="price">$${product.price}</span>
+                    <button class="add-to-cart" id="${product.id}">Agregar al carrito</button>
+                    </div>`;
+        }
+    );
+    $('#products-container').html(html);
+}
+
+//FUNCIÓN QUE CARGA LOS PRODUCTOS EN OFERTA DE LA HOME
+const buildCatalogOnSale = (arr) => {
+    let html ='';
+    let counter = 0;
+    arr.forEach(product => {
+            if (product.discount > 0) {
+                counter ++
+                html += `<div class="product">
+                            <span class="discount">${product.discount}% OFF</span>
+                            <figure>
+                                <img src="${product.imageURL}" width="100" alt="${product.name}">
+                            </figure>
+                            <h4>${product.name}</h4>
+                            <span class="price">$${product.price}</span>
+                            <button class="add-to-cart" id="${product.id}">Agregar al carrito</button>
+                        </div>`;
+            }
+        }
+    );
+
+    if (counter == 0) {
+        html += "<p>En este momento no tenemos productos en oferta</p>";
+    }
+    $('#products-container-onsale').html(html);
+}
+
+//FUNCIÓN PARA BUSCAR UN PRODUCTO YA AGREGADO EN EL CARRITO
+const searchProduct = (productId) => {
+  
+    if ( !sessionStorage.getItem("productsCart") ){
+      return false;
+    }
+  
+    let almacenados = JSON.parse(sessionStorage.getItem("productsCart"));
+    let encontrado = false;
+    let i = 0;
+    while (!encontrado && i != almacenados.length ){
+
+      if (almacenados[i].id == productId) {
+        encontrado = almacenados[i];
+      }
+  
+      i++;
+  
+    }
+  
+    return encontrado;
+  
+  }
+
+
+  //FUNCION PARA AGREGAR UN PRODUCTO COMPRADO AL CARRITO
+  const saveCartProduct = (newProduct) => {
+  
+    let item = sessionStorage.getItem("productsCart");
+    if (item) { //Pregunto si existe "productsCart" en sessionStorage
+  
+      let almacenados = JSON.parse(sessionStorage.getItem("productsCart"));
+      //console.log(newProduct);
+      almacenados.push(newProduct);
+
+      let almacenados_string = JSON.stringify(almacenados);
+      sessionStorage.setItem("productsCart", almacenados_string);
+  
+    } else { //Si no existe lo creo y agrego el producto nuevo
+  
+      let almacenados = new Array();
+      almacenados.push(newProduct);
+      let almacenados_string = JSON.stringify(almacenados);
+      sessionStorage.setItem("productsCart", almacenados_string);
+    }
+
+  }
+
+
+//FUNCION PARA GENERAR EL HTML DE TODOS LOS PRODUCTOS DEL CARRITO
+const cart = () => {
+
+    let item = sessionStorage.getItem("productsCart");
+    if (item) { //Pregunto si existe "productsCart" en sessionStorage
+        let almacenados = JSON.parse(sessionStorage.getItem("productsCart"));
+        let html = '';
+
+        for (let i=0; i < almacenados.length; i++) {
+            html +=`<div class="product" data-id="${almacenados[i].id}">
+                        <figure>
+                            <img src="${almacenados[i].imageURL}" width="100" alt="${almacenados[i].name}">
+                        </figure>
+                        <h4>${almacenados[i].name}</h4>
+                        <span class="price">$${almacenados[i].price}</span>
+                        <div class="my-3"><input class="quantity" type="number" id="cart-quantity-${almacenados[i].id}" name="quantity" min="1" max="99" value="${almacenados[i].saleAmount}" onchange="changeQuantity(${almacenados[i].id})"></div>
+                        <button type="button" onclick="deleteItemCart(${almacenados[i].id})" class="remove-to-cart"><i class="material-icons">delete</i></button>
+                    </div>`;
+        }
+
+        $('.cart-panel-content').html(html);
+        cartTotal();
+        document.querySelector('.cart-items').textContent = totalItems();
+
+    }
+}
+
+//FUNCIÓN QUE CALCULA EL VALOR TOTAL DE LA COMPRA
+const cartTotal = () => {
+    let almacenados = JSON.parse(sessionStorage.getItem("productsCart"));
+    let cartTotalAmount = document.querySelector(".total-amount span");
+    let cartTotal = 0;
+    
+    for (const product of almacenados) {
+        cartTotal += parseInt(product.price) * parseInt(product.saleAmount);
+    }
+
+    cartTotalAmount.textContent = cartTotal;
+
+}
+
+
+//FUNCIÓN PARA BORRAR ELEMENTOS DEL CARRITO
+const deleteItemCart = (id) => {
+
+    const productId = id;
+    let almacenados = JSON.parse(sessionStorage.getItem("productsCart"));
+
+    let products = document.querySelectorAll("#cart-panel .product");
+    products.forEach((element) => {
+        if (element.getAttribute('data-id') == id) {
+            $(element).fadeOut('slow', () =>{
+                $(element).remove();
+            });
+        }
+    });
+
+    const removeIndex = almacenados.findIndex((element) => { // Busco la posición del elemento modificado en el array de productos del carrito
+        return element.id == id;
+    });
+
+    almacenados.splice( removeIndex, 1 );
+    let almacenados_string = JSON.stringify(almacenados);
+    sessionStorage.setItem("productsCart", almacenados_string);
+    totalItems();
+    cartTotal();
+    document.querySelector('.cart-items').textContent = totalItems();
+}
+
+//FUNCIÓN PARA MOSTRAR EL TOTAL CUANDO SE CAMBIA LA CANTIDAD DE PRODUCTOS ELEGIDA
+const changeQuantity = (id) => {
+    let almacenados = JSON.parse(sessionStorage.getItem("productsCart"));
+    let quantity = document.getElementById("cart-quantity-" + id).value;
+
+    let productEnc = almacenados.filter((element) => {
+        return element.id == id;
+    });
+
+    const index = almacenados.findIndex((element) => { // Busco la posición del elemento modificado en el array de productos del carrito
+        return element.id == id;
+    });
+
+    productEnc[0].saleAmount = parseInt(quantity);
+
+    almacenados[index] = productEnc[0];
+    almacenados_string = JSON.stringify(almacenados);
+    sessionStorage.setItem("productsCart", almacenados_string);
+
+    cartTotal();
+    document.querySelector('.cart-items').textContent = totalItems();
+
+}
+
+const totalItems = () => {
+    let almacenados = JSON.parse(sessionStorage.getItem("productsCart"));
+    let totalItems = 0;
+
+    for (const product of almacenados) {
+        totalItems += parseInt(product.saleAmount);
+    }
+
+    return totalItems;
+}
---
+++ scripts/main.js
@@ -0,0 +1,120 @@
+//VARIABLES
+let products = [];
+const cartBtn = document.querySelector('.cart li a');
+const closeCart = document.querySelector('.btn-close');
+const removeItems = document.getElementsByClassName("remove-to-cart");
+
+
+//CLASES
+class Product {
+    constructor(id, name, price, type, stock, description, imageURL, discount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.stock = stock;
+        this.description = description;
+        this.imageURL = imageURL;
+        this.discount = discount;
+    }
+    
+    setNewStock(amount) {
+        this.stock = this.stock - amount;
+        return this.stock;
+    }
+}
+
+class UserVisitor {
+    constructor(age, name) {
+        this.age = age;
+        this.name = name;
+    }
+}
+
+class UserRegistered {
+    constructor(name, surname, email, pass) {
+        this.name= name;
+        this.surname = surname;
+        this.email = email;
+        this.pass = pass;
+    }
+}
+
+
+
+
+const wine_01 = new Product(1, "Estancia Mendoza", 340, "Malbec", 800, "Notas de cata: color rojo intenso con reflejos violáceos. Aromas y sabores a frutos rojos, ciruelas y frambuesas.", "images/estancia-mendoza-malbec.png", 0);
+const wine_02 = new Product(2, "FC Barcelona Malbec Reserva", 620, "Malbec", 1000, "Reserva 2020", "images/fc-barcelona-malbec-reserva.png", 0);
+const wine_03 = new Product(3, "Novecento", 258, "Malbec", 1500, "Bodega Dante Robino", "images/novecento-malbec.png", 15);
+const wine_04 = new Product(4, "Estancia Mendoza Sauvignon Blanc", 525, "Sauvignon Blanc", 500, "Notas de Cata: De color amarillo pálido, con tonos verdosos. En el aroma se perciben pomelo y durazno, muy intenso y refrescante. Es un vino suave, de acidez equilibrada y buena concentración. ", "images/estancia-mendoza-sauvignon-blanc.png", 30);
+const wine_05 = new Product(5, "Callia M Malbec", 239, "Malbec", 600, "Callia San Juan", "images/callia-malbec.png", 10);
+const wine_06 = new Product(6, "Fond De Cave", 495, "Malbec", 2500, "Bodega Trapiche", "images/fond-de-cave-malbec.png", 15);
+
+
+//CARGO LOS OBJETOS PRODUCTO EN EL ARRAY PRODUCTS
+products.push(wine_01, wine_02, wine_03, wine_04, wine_05, wine_06);
+
+
+
+if (!sessionStorage.getItem("edad")) {
+    let edad = prompt("Por favor ingrese su edad");
+    restrictionAge(edad);
+    sessionStorage.setItem("edad", edad);
+}
+
+document.querySelector('.overlay').style.display = 'none';
+
+//LLAMO A LAS FUNCIONES QUE ARMAR LOS CATALOGOS TANTO DE LA HOME (OFERTAS DEL DIA) COMO DE LA SECCIÓN NUESTROS VINOS
+buildCatalog(products);
+buildCatalogOnSale(products);
+cart();
+
+
+//AGREGAR PRODUCTOS AL CARRITO CUANDO SE HACE CLICK EN EL BOTÓN AGREGAR DE CADA PRODUCTO
+const buttonsAddCart = document.getElementsByClassName('add-to-cart');
+
+for (const button of buttonsAddCart) {
+
+    button.addEventListener('click', () => {
+
+        const id = button.getAttribute('id');
+
+        let productosEnc = products.filter((element) => {
+            return element.id == id;
+        });
+
+        let product = searchProduct(productosEnc[0].id);
+        let newProduct = productosEnc[0];
+        newProduct.saleAmount = 1;
+  
+        if (product != false) {
+            alert("El producto ya existe en el carrito ");
+        } else {
+            alert("¡Producto agregado exitosamente!");
+            saveCartProduct(newProduct);
+            cart(); //LLamo a la función cart para volver a generar el html del carrito con el nuevo producto agregado
+        }
+        
+    });
+}
+
+
+
+//ABRIR Y CERRAR CARRITO
+cartBtn.addEventListener('click', ()=> {
+    const cartPanel = document.getElementById("cart-panel");
+    cartBtn.classList.toggle('active');
+    cartPanel.classList.toggle('active');
+});
+
+
+closeCart.addEventListener('click', () => {
+    cartBtn.click(); 
+});
+
+
+//saveCartProduct(new Product(1, "Estancia Mendoza", 525, "Sauvignon Blanc", 500, "Notas de Cata: De color amarillo pálido, con tonos verdosos. En el aroma se perciben pomelo y durazno, muy intenso y refrescante. Es un vino suave, de acidez equilibrada y buena concentración. ", "images/estancia-mendoza-sauvignon-blanc.png", 30));
+//saveCartProduct(new Product(2, "Estancia Mendoza", 525, "Sauvignon Blanc", 500, "Notas de Cata: De color amarillo pálido, con tonos verdosos. En el aroma se perciben pomelo y durazno, muy intenso y refrescante. Es un vino suave, de acidez equilibrada y buena concentración. ", "images/estancia-mendoza-sauvignon-blanc.png", 30));
+
+
+
