3fa2e8c5884034aac1d9f4e65e6783904c9fe2cf
Matias Di Iorio <matiasmd8610@gmail.com>
2021-10-24 10:55:44 -0300

    Avances

--- scripts/functions.js
+++ scripts/functions.js
@@ -14,7 +14,7 @@ const restrictionAge = (age) => {
 const buildCatalog = (arr) => {
     let html ='';
     arr.forEach(product => {
-            html += `<div class="product">`;
+            html += `<div class="product" data-id="${product.id}">`;
             if (product.discount > 0) {
                 html += `<span class="discount">${product.discount}% OFF</span>` //ALT + 96
             }
@@ -37,7 +37,7 @@ const buildCatalogOnSale = (arr) => {
     arr.forEach(product => {
             if (product.discount > 0) {
                 counter ++
-                html += `<div class="product">
+                html += `<div class="product" data-id="${product.id}">
                             <span class="discount">${product.discount}% OFF</span>
                             <figure>
                                 <img src="${product.imageURL}" width="100" alt="${product.name}">
@@ -218,8 +218,153 @@ const totalItems = () => {
         if (totalItems > 0) {
             $('#end-transaction').show();
         } else {
+            $('#end-transaction').hide();
         }
     }
 
     return totalItems;
 }
+
+//FUNCIÓN PARA VALIDAR FORMULARIO DE COMPRA
+const formValidation = (e) => {
+    e.preventDefault();
+    let errors = 0;
+    
+    document.querySelectorAll('#checkout-form input').forEach((element) => {
+        element.classList.remove('error');
+    });
+
+    $('.error-msg').hide();
+    const name = document.getElementById('name');
+    const surname = document.getElementById('surname');
+    const email = document.getElementById('email');
+    const street = document.getElementById('street');
+    const number = document.getElementById('number');
+    const floor = document.getElementById('floor');
+    const city = document.getElementById('city');
+    const postalCode = document.getElementById('postal-code');
+    const province = document.getElementById('province');
+    const cardType = document.getElementById('card-type');
+    const cardNumber = document.getElementById('credit-card-number');
+    const cardName = document.getElementById('credit-card-name');
+    const expirationDate = document.getElementById('expiration-date');
+    const securityCode = document.getElementById('security-code');
+    let date_regex = /^(0[1-9]|1[012])\/\d{2}$/;
+
+    if (name.value == "") {
+      name.classList.add('error');
+      $('#name').next().show().html('Nombre incorrecto');
+      errors =+ 1;
+    }
+
+    if (surname.value == "") {
+      surname.classList.add('error');
+      $('#surname').next().show().html('Apellido incorrecto');
+      errors =+ 1;
+    }
+
+    if (email.value == "") {
+      email.classList.add('error');
+      $('#email').next().show().html('E-mail incorrecto');
+      errors =+ 1;
+    }
+
+    if (city.value == "") {
+      city.classList.add('error');
+      $('#city').next().show().html('Ciudad incorrecta');
+      errors =+ 1;
+    }
+
+    if (street.value == "") {
+      street.classList.add('error');
+      errors =+ 1;
+    }
+
+    if (postalCode.value == "") {
+        $('#postal-code').next().show().html('Código postal incorrecto');
+        postalCode.classList.add('error');
+        errors =+ 1;
+    }
+
+    if (floor.value != "" && isNaN(floor.value)) {
+      floor.classList.add('error');
+      $('#floor').next().show().html('Piso incorrecto');
+      errors =+ 1;
+    }
+
+    if (number.value == "" || isNaN(number.value)) {
+      number.classList.add('error');
+      $('#number').next().show().html('Número incorrecto');
+      errors =+ 1;
+    }
+
+    if (province.value == "0") {
+      province.classList.add('error');
+      $('#province').next().show().html('Debe seleccionar una provincia');
+      errors =+ 1;
+    }
+
+    if (cardType.value == "0") {
+        cardType.classList.add('error');
+        $('#card-type').next().show().html('Debe seleccionar forma de pago');
+        errors =+ 1;
+    }
+
+    if (cardName.value == "") {
+        cardType.classList.add('error');
+        $('#credit-card-name').next().css('display', 'inline-block').html('Debe completar su nombre tal como figura en la tarjeta');
+        errors =+ 1;
+    }
+
+    if (cardNumber.value == "" || isNaN(cardNumber.value)) {
+        cardNumber.classList.add('error');
+        $('#credit-card-number').next().show().html('Número de tarjeta inválido');
+        errors =+ 1;
+    }
+
+    if (expirationDate.value == "" || !date_regex.test(expirationDate.value)) {
+        expirationDate.classList.add('error');
+        $('#expiration-date').next().show().html('Fecha de expiración inválida');
+        errors =+ 1;
+    }
+
+    if (securityCode.value == "" || isNaN(securityCode.value)) {
+        expirationDate.classList.add('error');
+        $('#security-code').next().show().html('Código de seguridad inválido');
+        errors =+ 1;
+    }
+
+    if (errors > 0) {
+        return false;
+    } else {
+        $("#success-message").fadeIn();
+        setTimeout(() => {
+            $('.catalog-offers, .intro, #cart-panel, header .cart ul').fadeIn('fast');
+            $('#checkout, #success-message').fadeOut();
+            localStorage.removeItem('productsCart');
+            document.querySelector('.cart-items').textContent = "0";
+            $('.cart-panel-content').empty();
+            $('#cart-panel').removeClass('active');
+        }, 2000);
+        return true;
+    }
+    
+  }
+
+//FUNCIÓN PARA FILTRAR PRODUCTOS
+const filterType = (wineType) => {
+$.get("./data/products.json", function (response, state) {
+    if (state === "success") {
+        let productsResponse = response;
+        $('.product').hide();
+        for (const item of productsResponse) {
+            if (item.type == wineType) {
+                //console.log(wineType);
+                //console.log(item.id);
+                $('.product[data-id=' + item.id + ']').css('display', 'flex');
+            }
+        }  
+    }
+});
+}
+  
--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,9 @@ const cartBtn = document.querySelector('.cart li a');
 const closeCart = document.querySelector('.btn-close');
 const removeItems = document.getElementsByClassName("remove-to-cart");
 const buyBtn = document.getElementById("end-transaction");
-
+const keepBuyingyBtn = document.getElementById("btn-keep-buying");
+const checkoutForm = document.getElementById("checkout-form");
+const checkboxesType = document.querySelectorAll('input[name=type]');
 
 //CLASES
 class Product {
@@ -41,26 +43,9 @@ class UserRegistered {
     }
 }
 
-
-/*const wine_01 = new Product(1, "Estancia Mendoza", 340, "Malbec", "Notas de cata: color rojo intenso con reflejos violáceos. Aromas y sabores a frutos rojos, ciruelas y frambuesas.", "images/estancia-mendoza-malbec.png", 1000, 0);
-const wine_02 = new Product(2, "FC Barcelona Malbec Reserva", 620, "Malbec", "Reserva 2020", "images/fc-barcelona-malbec-reserva.png", 1000, 0);
-const wine_03 = new Product(3, "Novecento", 258, "Malbec", "Bodega Dante Robino", "images/novecento-malbec.png", 1000, 15);
-const wine_04 = new Product(4, "Estancia Mendoza Sauvignon Blanc", 525, "Sauvignon Blanc", "Notas de Cata: De color amarillo pálido, con tonos verdosos. En el aroma se perciben pomelo y durazno, muy intenso y refrescante. Es un vino suave, de acidez equilibrada y buena concentración. ", "images/estancia-mendoza-sauvignon-blanc.png", 1000, 30);
-const wine_05 = new Product(5, "Callia M Malbec", 239, "Malbec", "Callia San Juan", "images/callia-malbec.png", 1000, 10);
-const wine_06 = new Product(6, "Fond De Cave", 495, "Malbec", "Bodega Trapiche", "images/fond-de-cave-malbec.png", 1000, 15);
-
-
-//CARGO LOS OBJETOS PRODUCTO EN EL ARRAY PRODUCTS
-products.push(wine_01, wine_02, wine_03, wine_04, wine_05, wine_06);*/
-
-async function call_getProductsJson() {
-    await getProductsJson();
-}
-
-
-//FUNCION PARA PARA CARGAR LOS PRODUCTOS EN EL ARRAY DE PRODUCTOS USANDO LA CLASE PRODUCT COMO MODELO DE PRODUCTO POR AJAX $GET
-const getProductsJson = () => {
-    $.get("./data/products.json", function (response, state) {
+//FUNCION PARA CARGAR LOS PRODUCTOS EN EL ARRAY DE PRODUCTOS USANDO LA CLASE PRODUCT COMO MODELO DE PRODUCTO POR AJAX $GET
+async function getProductsJson() {
+    await $.get("./data/products.json", function (response, state) {
         if (state === "success") {
             let productsResponse = response;
             for (const item of productsResponse) {
@@ -68,13 +53,9 @@ const getProductsJson = () => {
                 products.push(newProduct);
             }  
         }
-        console.log(products);
     });
+    //console.log(products);
 }
-
-
-call_getProductsJson();
-console.log(products);
 
 
 if (!localStorage.getItem("edad")) {
@@ -85,38 +66,40 @@ if (!localStorage.getItem("edad")) {
 
 document.querySelector('.overlay').style.display = 'none';
 
-//LLAMO A LAS FUNCIONES QUE ARMAR LOS CATALOGOS TANTO DE LA HOME (OFERTAS DEL DIA) COMO DE LA SECCIÓN NUESTROS VINOS
-buildCatalog(products);
-buildCatalogOnSale(products);
+
 cart('.cart-panel-content');
 
 
 //AGREGAR PRODUCTOS AL CARRITO CUANDO SE HACE CLICK EN EL BOTÓN AGREGAR DE CADA PRODUCTO
-const buttonsAddCart = document.getElementsByClassName('add-to-cart');
+const addProductsCart = () => {
 
-for (const button of buttonsAddCart) {
-
-    button.addEventListener('click', () => {
-
-        const id = button.getAttribute('id');
-
-        let productosEnc = products.filter((element) => {
-            return element.id == id;
+    const buttonsAddCart = document.getElementsByClassName('add-to-cart');
+    
+    for (const button of buttonsAddCart) {
+    
+        button.addEventListener('click', () => {
+    
+            const id = button.getAttribute('id');
+    
+            let productosEnc = products.filter((element) => {
+                return element.id == id;
+            });
+    
+            let product = searchProduct(productosEnc[0].id);
+            let newProduct = productosEnc[0];
+            newProduct.saleAmount = 1;
+      
+            if (product != false) {
+                alert("El producto ya existe en el carrito ");
+            } else {
+                alert("¡Producto agregado exitosamente!");
+                saveCartProduct(newProduct);
+                cart('.cart-panel-content'); //LLamo a la función cart para volver a generar el html del carrito con el nuevo producto agregado
+            }
+            
         });
+    }
 
-        let product = searchProduct(productosEnc[0].id);
-        let newProduct = productosEnc[0];
-        newProduct.saleAmount = 1;
-  
-        if (product != false) {
-            alert("El producto ya existe en el carrito ");
-        } else {
-            alert("¡Producto agregado exitosamente!");
-            saveCartProduct(newProduct);
-            cart('.cart-panel-content'); //LLamo a la función cart para volver a generar el html del carrito con el nuevo producto agregado
-        }
-        
-    });
 }
 
 
@@ -144,8 +127,52 @@ $('h1').animate({
 buyBtn.addEventListener('click', () => {
     $('.catalog-offers, .intro, #cart-panel, header .cart ul').fadeOut('fast');
     $('#checkout').fadeIn();
-})
+});
+
+//BOTON SEGUIR COMPRANDO
+keepBuyingyBtn.addEventListener('click', () => {
+    $('.catalog-offers, .intro, #cart-panel, header .cart ul').fadeIn('fast');
+    $('#checkout').fadeOut();
+});
+
+async function call_getProductsJson() {
+    await getProductsJson();
+    
+    //LLAMO A LAS FUNCIONES QUE ARMAR LOS CATALOGOS TANTO DE LA HOME (OFERTAS DEL DIA) COMO DE LA SECCIÓN NUESTROS VINOS
+    await buildCatalog(products);
+    await buildCatalogOnSale(products);
+
+    await addProductsCart();
+    //console.log(products);
+}
+
+call_getProductsJson();
+//console.log(products);
+
+
+checkoutForm.addEventListener('submit', formValidation);
 
 
 
+checkboxesType.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (element.checked) {
+            //console.log(element.value);
+            filterType(element.value);
+        } else {
+            $('.product').css('display','flex');
+        }
+    })
+});
 
+
+/*const wine_01 = new Product(1, "Estancia Mendoza", 340, "Malbec", "Notas de cata: color rojo intenso con reflejos violáceos. Aromas y sabores a frutos rojos, ciruelas y frambuesas.", "images/estancia-mendoza-malbec.png", 1000, 0);
+const wine_02 = new Product(2, "FC Barcelona Malbec Reserva", 620, "Malbec", "Reserva 2020", "images/fc-barcelona-malbec-reserva.png", 1000, 0);
+const wine_03 = new Product(3, "Novecento", 258, "Malbec", "Bodega Dante Robino", "images/novecento-malbec.png", 1000, 15);
+const wine_04 = new Product(4, "Estancia Mendoza Sauvignon Blanc", 525, "Sauvignon Blanc", "Notas de Cata: De color amarillo pálido, con tonos verdosos. En el aroma se perciben pomelo y durazno, muy intenso y refrescante. Es un vino suave, de acidez equilibrada y buena concentración. ", "images/estancia-mendoza-sauvignon-blanc.png", 1000, 30);
+const wine_05 = new Product(5, "Callia M Malbec", 239, "Malbec", "Callia San Juan", "images/callia-malbec.png", 1000, 10);
+const wine_06 = new Product(6, "Fond De Cave", 495, "Malbec", "Bodega Trapiche", "images/fond-de-cave-malbec.png", 1000, 15);
+
+
+//CARGO LOS OBJETOS PRODUCTO EN EL ARRAY PRODUCTS
+products.push(wine_01, wine_02, wine_03, wine_04, wine_05, wine_06);*/
